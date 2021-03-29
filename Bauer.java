c2066a2a3efad9292836201881e53fc4f55e40bc
Michael Kluth <michael.kluth@wkdo.info>
2021-03-29 13:19:45 +0200

    Bauer auch in schwarz möglich. Dazu verschiedene Konstruktoren hinzugefügt.

--- Bauer.java
+++ Bauer.java
@@ -15,10 +15,18 @@ public class Bauer {
   GLZylinder fuss;
   
   public Bauer() {
-    this(0, 0);
+    this(0, 0, true);
+  }
+  
+  public Bauer(boolean pWeiss) {
+    this (0, 0, pWeiss);
   }
   
   public Bauer(double pX, double pZ) {
+    this(pX, pZ, true);
+  }
+  
+  public Bauer(double pX, double pZ, boolean pWeiss) {
     kopf = new GLKugel(pX, 90 + 80 + 18, pZ, 40);
     hals = new GLTorus(pX, 90 + 70, pZ, 30, 5);
     hals.drehe(90, 0, 0);
@@ -32,6 +40,15 @@ public class Bauer {
     beine.drehe(90, 0, 0);
     fuss = new GLZylinder(pX, 5.0, pZ, 80.0, 10.0);
     fuss.drehe(90, 0, 0);
+    if (!pWeiss) {
+      kopf.setzeFarbe(0.1, 0.1, 0.1);
+      hals.setzeFarbe(0.1, 0.1, 0.1);
+      oberkoerper.setzeFarbe(0.1, 0.1, 0.1);
+      guertel.setzeFarbe(0.1, 0.1, 0.1);
+      unterkoerper.setzeFarbe(0.1, 0.1, 0.1);
+      beine.setzeFarbe(0.1, 0.1, 0.1);
+      fuss.setzeFarbe(0.1, 0.1, 0.1);
+    }
   }
   
 }
--- Umgebung.java
+++ Umgebung.java
@@ -1,6 +1,7 @@
 import GLOOP.GLLicht;
 import GLOOP.GLEntwicklerkamera;
 import GLOOP.GLHimmel;
+import GLOOP.GLBoden;
 /**
  * Beschreiben Sie hier die Klasse Umgebung.
  * 
@@ -11,10 +12,15 @@ public class Umgebung {
 
   GLEntwicklerkamera fenster; 
   GLLicht licht; 
+  GLBoden boden;
+  GLHimmel himmel;
+  
   public Umgebung() {
     fenster = new GLEntwicklerkamera();
     fenster.zeigeAchsen(true);
     fenster.setzePosition(500, 1000, 500);
     licht = new GLLicht();
+    boden = new GLBoden("boden.jpg");
+    himmel = new GLHimmel("himmel.jpg");
   }
 }
