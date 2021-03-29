5771b407f6aeb8ff469c22f39e46446326dd37f3
Michael Kluth <michael.kluth@wkdo.info>
2021-03-29 12:54:38 +0200

    Parameter in den Konstrukturen von Turm und Bauer hinzugefügt, um die Postition zu setzen.

--- Bauer.java
+++ Bauer.java
@@ -15,18 +15,22 @@ public class Bauer {
   GLZylinder fuss;
   
   public Bauer() {
-    kopf = new GLKugel(0, 90 + 80 + 18, 0, 40);
-    hals = new GLTorus(0, 90 + 70, 0, 30, 5);
+    this(0, 0);
+  }
+  
+  public Bauer(double pX, double pZ) {
+    kopf = new GLKugel(pX, 90 + 80 + 18, pZ, 40);
+    hals = new GLTorus(pX, 90 + 70, pZ, 30, 5);
     hals.drehe(90, 0, 0);
-    oberkoerper = new GLKegelstumpf(0, 90 + 35, 0, 40, 15, 80);
+    oberkoerper = new GLKegelstumpf(pX, 90 + 35, pZ, 40, 15, 80);
     oberkoerper.drehe(90, 0, 0);
-    guertel = new GLTorus(0,92,0,46,2);
+    guertel = new GLTorus(pX, 92, pZ, 46, 2);
     guertel.drehe(90, 0, 0);
-    unterkoerper = new GLKegelstumpf(0, 30 + 30, 0, 70, 50, 60);
+    unterkoerper = new GLKegelstumpf(pX, 30 + 30, pZ, 70, 50, 60);
     unterkoerper.drehe(90,0,0);
-    beine = new GLTorus(0, 10 + 10, 0, 60, 20);
+    beine = new GLTorus(pX, 10 + 10, pZ, 60, 20);
     beine.drehe(90, 0, 0);
-    fuss = new GLZylinder(0.0, 5.0, 0.0, 80.0, 10.0);
+    fuss = new GLZylinder(pX, 5.0, pZ, 80.0, 10.0);
     fuss.drehe(90, 0, 0);
   }
   
--- Turm.java
+++ Turm.java
@@ -6,8 +6,7 @@ import GLOOP.*;
  * @version 19.3.2021
  */
 public class Turm {
-  //GLKugel kopf;
-  //GLTorus hals;
+  
   Bogen zinne1, zinne2, zinne3, zinne4;
   
   GLZylinder dach;
@@ -16,29 +15,34 @@ public class Turm {
   GLKegelstumpf sockel; //der unter teil
   GLZylinder fuss;
   
+  
   public Turm() {
-    stuetze = new GLKegelstumpf(200, 200, 0, 50, 55, 20);
+    this(200, 0);
+  }
+  
+  public Turm(double pX, double pZ) {
+    stuetze = new GLKegelstumpf(pX, 200, pZ, 50, 55, 20);
     stuetze.drehe(90, 0, 0);
-    mitte = new GLKegelstumpf(200, 10 + 70 + 55, 0, 70, 50, 110);
+    mitte = new GLKegelstumpf(pX, 10 + 70 + 55, pZ, 70, 50, 110);
     mitte.drehe(90,0,0);
-    sockel = new GLKegelstumpf(200, 10 + 35, 0, 89, 70, 70);
+    sockel = new GLKegelstumpf(pX, 10 + 35, pZ, 89, 70, 70);
     sockel.drehe(90, 0, 0);
     //wie bauer
-    fuss = new GLZylinder(200, 5, 0, 90, 10);
+    fuss = new GLZylinder(pX, 5, pZ, 90, 10);
     fuss.drehe(90, 0, 0);
-    dach = new GLZylinder(200, 210 + 20, 0, 60, 40);
+    dach = new GLZylinder(pX, 210 + 20, pZ, 60, 40);
     dach.drehe(90,0,0);
     
-    zinne1 = new Bogen(200, 250 + 5, 0, 70, 60, 55, 10);
+    zinne1 = new Bogen(pX, 250 + 5, pZ, 70, 60, 55, 10);
     zinne1.drehe(90, -10, 0);
     
-    zinne2 = new Bogen(200, 250 + 5, 0, 70, 60, 55, 10);
+    zinne2 = new Bogen(pX, 250 + 5, pZ, 70, 60, 55, 10);
     zinne2.drehe(-90, 10, 0);
     
-    zinne3 = new Bogen(200, 250 + 5, 0, 70, 60, 55, 10);
+    zinne3 = new Bogen(pX, 250 + 5, pZ, 70, 60, 55, 10);
     zinne3.drehe(-90, 190, 0);
     
-    zinne4 = new Bogen(200, 250 + 5, 0, 70, 60, 55, 10);
+    zinne4 = new Bogen(pX, 250 + 5, pZ, 70, 60, 55, 10);
     zinne4.drehe(90, 170, 0);
   }
   
