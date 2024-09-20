de99e27b727067feac1e5a54d708635324dcc61f
Michael Kluth <michael.kluth@wkdo.info>
2024-09-20 09:02:47 +0200

    Klasse Pfeerd hinzugefügt

---
+++ Pferd.java
@@ -0,0 +1,16 @@
+import GLOOP.*;
+/**
+ * Basis-Ansicht für die Figur Turm.
+ * 
+ * @author Michael Kluth
+ * @version 20.9.2024
+ */
+public class Pferd { 
+ 
+  public Pferd()
+  {
+    
+  }
+
+  
+}
